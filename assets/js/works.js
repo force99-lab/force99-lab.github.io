afea2763e0a2de0531739f62f10f5c1ff4919ddb
force99-lab <vv6z8tzj6j@privaterelay.appleid.com>
2026-08-17 17:01:50 +0800

    add cover: 匀变速位移逼近 封面

--- assets/js/works.js
+++ assets/js/works.js
@@ -461,7 +461,7 @@ const WORKS = [
   {
     type:"animation", title:"匀变速位移逼近",
     desc:"匀变速直线运动中用矩形条逼近位移，直观演示极限思想与 v-t 图面积含义。",
-    icon:"📐", link:"animations/uniform-acceleration-displacement.html", date:"2026-08-17", tag:"运动学"
+    icon:"📐", cover:"assets/img/uniform-acceleration-displacement-cover.png", link:"animations/uniform-acceleration-displacement.html", date:"2026-08-17", tag:"运动学"
   },
 
   /* ---------------- 解题 ---------------- */
