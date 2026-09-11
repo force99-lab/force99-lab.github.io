951ef34bce1c26a7ae543a426a16e92626638881
force99-lab <vv6z8tzj6j@privaterelay.appleid.com>
2026-09-11 14:39:32 +0800

    remove duplicate henan15 solve card

--- assets/js/works.js
+++ assets/js/works.js
@@ -3688,12 +3688,7 @@ const WORKS = [
     icon:"📝", cover:"assets/img/henan15-ball-shell-cover.png",
     link:"solutions/henan15-ball-shell-collision.html", date:"2026-09-11", tag:"力学"
   },
-  {
-    type:"solve", title:"球壳与小球竖直弹性碰撞 · 三页式讲解",
-    icon:"📝", cover:"assets/img/henan15-ball-shell-cover.png",
-    link:"solutions/henan15-ball-shell-collision.html", date:"2026-09-11", tag:"力学"
-  }
-];
+  ];
 
 
 
